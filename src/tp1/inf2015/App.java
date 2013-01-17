c67763393b61ca06afebda515ab2b0455252ac07
Simon <simon.legault.hebert@gmail.com>
2013-01-17 18:37:55 -0500

    Premier commit

---
+++ src/tp1/inf2015/App.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp1.inf2015;
+
+/**
+ *
+ * @author Leg
+ */
+public class App {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
